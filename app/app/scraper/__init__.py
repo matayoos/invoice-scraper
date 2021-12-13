7e02b433ddc9ecc0018174981573eb2550655884
Matheus Goncalves <matayoos@protonmail.com>
2021-12-13 17:35:42 -0300

    Fixing a typo

---
+++ app/app/scraper/__init__.py
@@ -0,0 +1,2 @@
+from .base import get_content
+from .scraper_grocery_store import get_grocery_store_info
--- app/app/scrapper/__init__.py
+++
@@ -1,2 +0,0 @@
-from .base import get_content
-from .scrapper_grocery_store import get_grocery_store_info
